f4e79e5983d4b913ffb9468d0c23717cbbc871bb
S.J.A.S.M.S.Jayasinghe <88881292+IT20222154@users.noreply.github.com>
2021-09-16 18:52:16 +0530

    Update HeroSection.js

--- client/src/home/components/HeroSection.js
+++ client/src/home/components/HeroSection.js
@@ -80,9 +80,7 @@ function HeroSection() {
             </Typography>
             <Typography variant="h6" className={classes.h4}>
               The human body has been designed to resist an infinite number of
-              changes and attacks brought about by its environment. The secret
-              of good health lies in successful adjustment to changing stresses
-              on the body
+              changes and attacks brought about by its environment.
             </Typography>
             <Link to="/login">
               <Button className={classes.button}>Register Now</Button>
